199ad497d0c6e0a4db5a97f86544cd1ffd445ab5
融雪时分 <cutedays@icloud.com>
2021-04-11 22:41:17 +0800

    Update CuteRendererUtility.py

--- src/utility/CuteRendererUtility.py
+++ src/utility/CuteRendererUtility.py
@@ -241,7 +241,7 @@ class CuteRendererUtility:
 
             #radius = 0.0
             radius = determine_light_radius(area)
-            energy = determine_light_energy(area, base_energy=190.0, base_area=25.0, area_factor=8.0, clamp_min=60, clamp_max=350.)
+            energy = determine_light_energy(area, base_energy=200.0, base_area=25.0, area_factor=8.0, clamp_min=60, clamp_max=350.)
             return [[x_corrected, y_corrected, z_center - radius], energy, radius]
 
         # def add_light_triangles(ceiling):
